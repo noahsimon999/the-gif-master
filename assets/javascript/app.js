8bda86b66b686bf92a0f1a6d70828e297005c15b
nohabroha <noahysimon@gmail.com>
2018-07-04 16:17:08 -0500

    basicaly working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,10 +9,33 @@ $(document).ready(function () {
 
 // display list of gif buttons
 var foods = ["pizza", "pasta", "pork", "ribs", "hamburgers", "hotdogs", "french fries"]
-for(var i = 0; i < foods.length; i++) {
-    $(".buttons").append("<button>" + foods[i] + "</button>")
+var a = 0;
+renderButtons();
+
+function renderButtons() {
+    for(a; a < foods.length; a++) {
+        $(".buttons").append("<button>" + foods[a] + "</button>")
+        GIFMechanics();
+    }
 }
 
+
+// add more foods to the array
+$("#add-food").on("click", function(event) {
+    event.preventDefault();
+
+    // This line grabs the input from the textbox
+    var moreFood = $("#food-input").val().trim();
+
+    // Adding the movie from the textbox to our array
+    foods.push(moreFood);
+    renderButtons();
+    a = foods.length;
+  });
+
+
+// listens for a button click and then returns gifs
+function GIFMechanics() {
 var userChoice = foods[0];
 $(".buttons button").click(function() {
     userChoice = $(this).text();
@@ -22,16 +45,14 @@ $(".buttons button").click(function() {
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
+    }).then(function(response) {
         for(var i = 0; i < 11; i++) {
-            $(".gifs").append("<img src='" + response.data[i].images.fixed_height.url+ "'>")
+            $(".gifs").append("<img src='" + response.data[i].images.fixed_height.url + "'>");
+            $(".gifs").append("<p>" + response.data[i].rating+ "</p>");
         }
-      });
-
-
+    });
 })
-
-
+}
 
 
 
